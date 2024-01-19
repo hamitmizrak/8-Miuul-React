9fe8ffd44759805bb436e251056ee022b47a8713
Hamit Mizrak <hamitmizrak@gmail.com>
2024-01-19 20:52:27 +0300

    View

--- src/components/MiuulMain.jsx
+++ src/components/MiuulMain.jsx
@@ -1,12 +1,17 @@
 //rfc
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
+import myViewPicture from "../img/dünya.png";
+
+
+// EXPORT
 export default function MiuulMain() {
   return (
-    <div style={{marginTop:"200px"}}>
-
-      <Link className="btn btn-primary" to="miuul/spa/list">List</Link>
+    <div className="w-75" style={{ marginTop: "200px" }}>
+      <Link to="/miuul/spa/list">
+        <img src={myViewPicture} className="w-75" />
+      </Link>
     </div>
-  )
+  );
 }
--- src/components/miuulspa/MiuulSpaView.jsx
+++ src/components/miuulspa/MiuulSpaView.jsx
@@ -1,7 +1,66 @@
-import React from 'react'
+// React
+import React, { useEffect, useState } from "react";
 
-export default function MiuulSpaView() {
+// Navigate
+import { useNavigate, useParams } from "react-router-dom";
+
+// İmage
+import myViewPicture from "../../img/dünya.png";
+
+// Api
+import MiuulNodeJsMongoDbReactApi from "../../services/MiuulNodeJsMongoDbReactApi";
+
+// FUNCTION
+function CategoryView(props) {
+  // REDIRECT
+  let navigate = useNavigate();
+
+  // STATE
+  const [id, setId] = useState(null);
+  const [miuulViewStateApi, setMiuulViewStateApi] = useState([]); //dikkat diziyi unutma
+
+  // PARAMS (ID)
+  const redirectHandlingViewId = useParams();
+
+  // USE EFFECT
+  useEffect(() => {
+    // 1.YOL (ID)
+    // setId(localStorage.getItem("category_view_id"));
+    setId(redirectHandlingViewId.id); //2.YOL (ID)
+
+    //FIND
+    MiuulNodeJsMongoDbReactApi.miuulSpaApiFindById(redirectHandlingViewId.id)
+      .then((response) => {
+        console.log(response.data);
+        setMiuulViewStateApi(response.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []); //end use effect
+
+  /////////////////////////////////////////
+
+  // RETURN
   return (
-    <div style={{marginTop:"2rem"}}>MiuulSpaView</div>
-  )
-}
+    <React.Fragment>
+      <h1 className="text-center display-3 mt-5">VIEW</h1>
+      <div className="card text-center w-75">
+        <img
+          className="card-img-top w-50  ms-5 "
+          src={myViewPicture}
+          alt="Title"
+        />
+        <div className="card-body">
+          <h4 className="card-title">{miuulViewStateApi._id}</h4>
+          <p className="card-text">{miuulViewStateApi.title}</p>
+          <p className="card-text">{miuulViewStateApi.counter}</p>
+          <p className="card-text">{miuulViewStateApi.description}</p>
+        </div>
+      </div>
+    </React.Fragment>
+  ); //end return
+} // end function
+
+// EXPORT
+export default CategoryView;
